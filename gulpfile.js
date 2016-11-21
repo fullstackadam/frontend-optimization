0c4f42210a324e995760fd162bd1f05a5f8d9677
Adam McMahon <obelesk411@users.noreply.github.com>
2016-11-21 13:34:34 -0700

    chore: set up minification for html, css and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,42 @@
 var gulp = require('gulp'),
-	htmlBeautify = require('gulp-prettify'),
+	/*htmlBeautify = require('gulp-prettify'),
 	cssBeautify = require('gulp-cssbeautify'),
 	jsBeautify = require('gulp-beautify'),
-	jshint = require('gulp-jshint');
+	jshint = require('gulp-jshint');*/
+  //minify
+  htmlMinify = require('gulp-htmlmin'),
+  cssMinify = require('gulp-clean-css');
+  jsMinify = require('gulp-jsmin');
+
+gulp.task('htmlMinify', function() {
+  gulp.src('src/*.html')
+    .pipe(htmlMinify({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+  gulp.src('src/views/*.html')
+    .pipe(htmlMinify({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('cssMinify', function() {
+  gulp.src('src/css/*')
+    .pipe(cssMinify())
+    .pipe(gulp.dest('dist/css'));
+  gulp.src('src/views/css/*')
+    .pipe(cssMinify())
+    .pipe(gulp.dest('dist/views/css'));
+});
+
+gulp.task('jsMinify', function() {
+  gulp.src('src/js/*')
+    .pipe(jsMinify())
+    .pipe(gulp.dest('dist/js'));
+  gulp.src('src/views/js/*')
+    .pipe(jsMinify())
+    .pipe(gulp.dest('dist/views/js'));
+});
 
 gulp.task('htmlBeautify', function(){
-  gulp.src('src/index.html')
+  gulp.src('src/*.html')
     .pipe(htmlBeautify({indent_size: 4}))
     .pipe(gulp.dest('dist'));
 });
@@ -13,13 +44,13 @@ gulp.task('htmlBeautify', function(){
 gulp.task('cssBeautify', function(){
   gulp.src('src/css/*')
     .pipe(cssBeautify({indent: '    '}))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('src/css'));
 });
 
 gulp.task('jsBeautify', function(){
   gulp.src('src/js/*.js')
     .pipe(jsBeautify({indentSize: 4}))
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('src/js'));
 });
 
 gulp.task('lint', function(){
@@ -31,4 +62,4 @@ gulp.task('lint', function(){
 gulp.src('src/images/*')
     .pipe(gulp.dest('dist/images'));
 
-gulp.task('default', ['htmlBeautify', 'cssBeautify', 'jsBeautify', 'lint']);
+gulp.task('default', ['htmlMinify', 'cssMinify', 'jsMinify']);
